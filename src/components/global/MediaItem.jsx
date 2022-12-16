63d4b0ed52062c168d5c47927f9ba74f62b19413
aalend <aalendemirov@gmail.com>
2022-12-16 22:16:41 +0100

    Adding hlaf star rating to MediaItem

--- src/components/global/MediaItem.jsx
+++ src/components/global/MediaItem.jsx
@@ -1,6 +1,5 @@
-import { StarIcon } from '@heroicons/react/20/solid';
 import { TMDB_BACKDROP_PATH } from '../../config/config';
-import Rating from '../rating/Rating';
+import StarRating from '../rating/StarRating';
 
 function MediaItem({ name, backdrop, description, voteAverage, voteCount }) {
   return (
@@ -11,10 +10,10 @@ function MediaItem({ name, backdrop, description, voteAverage, voteCount }) {
         className='aspect-video w-full rounded-3xl object-cover'
       />
       <div className='py-4'>
-        <p className='flex items-center gap-4'>
-          <Rating voteAverage={voteAverage} />
+        <div className='flex items-center gap-4'>
+          <StarRating voteAverage={voteAverage} />
           <span className='text-slate-400'>{voteCount} votes</span>
-        </p>
+        </div>
         <h2 className='mt-4'>{name}</h2>
         <p className='truncate text-slate-400'>{description}</p>
       </div>
