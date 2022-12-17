b931a488972d509ac2cd616bc5d54d9137231217
aalend <aalendemirov@gmail.com>
2022-12-17 16:23:08 +0100

    completing series page, adding filter by categories.

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import { apiSlice } from '../features/movies/movies-slice';
+import { seriesApiSlice } from '../features/tv-series/tv-series-slice';
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    [seriesApiSlice.reducerPath]: seriesApiSlice.reducer,
   },
+
   middleware: getDefaultMiddleware => {
-    return getDefaultMiddleware().concat(apiSlice.middleware);
+    return getDefaultMiddleware().concat(apiSlice.middleware, seriesApiSlice.middleware);
   },
 });
